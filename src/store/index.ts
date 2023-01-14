94f7580368b918c0bf97316817e0a59056b61dd3
Biskwit <corentin.gorski@exaion.com>
2023-01-14 16:24:01 +0100

    clear

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,18 +2,10 @@ import type { InjectionKey } from 'vue';
 import { createStore } from 'vuex';
 import type { Store } from 'vuex';
 import createPersistedState from "vuex-persistedstate"
-import { Instance } from '../@types/Instance';
-import { Volume } from '../@types/Volume';
-import { sshKey } from '../@types/sshKey';
-import { LoadBalancer } from '../@types/LoadBalancer';
 
 export interface State {
     authToken: string;
     logged: boolean;
-    instances: Instance[];
-    volumes: Volume[];
-    SSHKeys: sshKey[];
-    loadBalancer: LoadBalancer[];
 }
 
 export const key: InjectionKey<Store<State>> = Symbol();
@@ -22,10 +14,6 @@ export const store = createStore<State>({
     state: {
         authToken: "",
         logged: false,
-        instances: [],
-        volumes: [],
-        SSHKeys: [],
-        loadBalancer: []
     },
     mutations: {
         setAuthToken(state, newToken) {
@@ -33,15 +21,6 @@ export const store = createStore<State>({
         },
         setLogged(state, val) {
             state.logged = val
-        },
-        setInstances(state, val) {
-            state.instances = val
-        },
-        setVolumes(state, val) {
-            state.volumes = val
-        },
-        setSSHKeys(state, val) {
-            state.SSHKeys = val
         }
     },
     plugins: [createPersistedState({
